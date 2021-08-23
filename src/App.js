d8b261eeeb524c0b975b660d9a6d8895897890c2
ttucker <tracytuckeremail@gmail.com>
2021-08-23 03:55:56 -0500

    Base ccharacter creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+    thumb: '/images/gary.png'
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+    thumb: '/images/cato.png'
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+    thumb: '/images/kvn.png'
+  },
+  {
+    id: 'mooncake',
+    name: 'Mooncake',
+    thumb: '/images/mooncake.png'
+  },
+  {
+    id: 'quinn',
+    name: 'Quinn Ergon',
+    thumb: '/images/quinn.png'
+  }
+]
+
 function App() {
   return (
     <div className="App">
